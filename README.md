2808252053c4475965022305063c44c8103aacf3
Raiane Carvalho <raiane_cj@hotmail.com>
2025-07-14 15:13:54 -0300

    Update: promises

--- README.md
+++ README.md
@@ -1,15 +1,16 @@
 # Curso Testes de Aplicações Modernas com Cypress do Udemy - Francisco Wagner
 
 ## Arrumando o Ambiente
+Ferramentas necessárias:
  - Node.js
  - VS Code
  - Cypress
 
 1 - Iniciar projeto com o comando "npm init -y" no terminal
-### npm init -y
+#### npm init -y
 
 2 - Adicionar o cypress como dependencia do projeto (instalar):
-### npm install cypress
+#### npm install cypress
 
 3 - Abrir o Cypress:
 Adicionar no arquivo package.json 
@@ -20,10 +21,11 @@ Adicionar no arquivo package.json
   },
 
 E depois rodar o comando
-### npm run open
+#### npm run open
 
 ## Iniciando os estudos
 
 Conceitos do JS importantes de saber:
  - Arrow Functions
  - Promises
+ - Describe / It
--- cypress/e2e/2-advanced-examples/promises.cy.js
+++ cypress/e2e/2-advanced-examples/promises.cy.js
@@ -1,19 +1,42 @@
 // Promises
+// Promises são objetos que representam a eventual conclusão (ou falha) de uma operação assíncrona e seu valor resultante.
 
 it('sem testes, ainda', () => {})
 
+// Demonstração básica de como uma função assíncrona funciona com callbacks
 
-// Deixar o metodo como assincrono, assim a requisição pode ser aguardada enquanto o teste roda
-const getSomething = (callback) => {
-    setTimeout(() => {
-        callback(12)
-    },1000)
+// const getSomething = (callback) => {
+//     setTimeout(() => {
+//         callback(12)
+//     },1000)
+// }
+
+// const system = () => {
+//     console.log('init')
+//     getSomething(some => console.log(`Something is ${some}`))
+//     console.log('end')
+// }
+
+// system()
+
+// A Promise é um objeto que pode ser usado para encadear operações assíncronas de forma mais legível.
+// Aqui, usamos uma Arrow Function para definir a função que retorna uma Promise.
+const getSomething = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(13)
+        }, 1000)
+    })
 }
 
 const system = () => {
     console.log('init')
-    getSomething(some => console.log(`Something is ${some}`))
+    const prom = getSomething()
+    prom.then(some => {
+        console.log(`Something is ${some}`)
+    })
     console.log('end')
 }
 
 system()
+
